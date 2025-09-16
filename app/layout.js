f7f00af6b801944d57f43b63121779abdb4c7d15
ShaikRurian007 <shabana54671@gmail.com>
2025-09-17 03:02:11 +0530

    feat: implement ClientProviders and FooterWrapper components in layout for improved structure and client-side rendering

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Footer from "./components/footer";
+import { ClientProviders } from "./components/client-providers";
+import FooterWrapper from "./components/footer-wrapper";
 import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
@@ -19,13 +18,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          <Navbar />
-          {children}
-          <ScrollToTop />
-        </main>
-        <Footer />
+        <ClientProviders>
+          <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+            <Navbar />
+            {children}
+            <ScrollToTop />
+          </main>
+          <FooterWrapper />
+        </ClientProviders>
       </body>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
